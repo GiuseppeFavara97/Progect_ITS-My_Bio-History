ad9914e295692eba03ffae8970163ede028c62a3
GiuseppeFavara97 <giuseppe.favara1997@gmail.com>
2025-05-14 11:19:05 +0200

    creata funzione

--- reversingStrings.ts
+++ reversingStrings.ts
@@ -1,4 +1,6 @@
-const message:string = "helloworld";
+const message:string = "stressed";
+const message1:string = "strops";
+const message2:string = "racecar";
 
 //one way with built in functions
 function reverseStringBuildIn(str){
@@ -6,24 +8,7 @@ function reverseStringBuildIn(str){
     return mystr;
 }
 
-function reverseStringWithLoop(str){
-    var mystr =  "";
-    for(let i = str.length-1;i>=0;i--){
-        mystr += str[i];
-    }
-    
-    return mystr;
-}
 
-//Time Consuming Process if string is lengthy 
-function reverseStringWithRecursion(str){
-   if(str ===""){
-        return str;       
-   }else{
-       return reverseStringWithRecursion(str.substr(1))+str.charAt(0);
-   }
-}
-
-console.log("With Predefined Functions : "+message+" to "+ reverseStringBuildIn(message));
-console.log("With Loop : "+message+" to " + reverseStringWithLoop(message));
-console.log("With Recursion : "+message+" to " + reverseStringWithRecursion(message));
+console.log("revers string: "+message+" to "+ reverseStringBuildIn(message));
+console.log("revers string: "+message1+" to "+ reverseStringBuildIn(message1));
+console.log("revers string: "+message2+" to "+ reverseStringBuildIn(message2));
