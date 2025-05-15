d0627c3826149fa455cd72522da3b831b17b92bb
michele <michele@beehive@gmail.com>
2025-05-15 12:32:50 +0200

    create class medical_records

--- reversingStrings.js
+++ reversingStrings.js
@@ -1,4 +1,8 @@
 function reverseString(str) {
     return str.split('').reverse().join('');
 }
+<<<<<<< HEAD
 console.log(reverseString("stressed"));
+=======
+console.log(reverseString("strops"));
+>>>>>>> d98a5b3e9c971f97fa42cc92f3f77b7871259e71
--- reversingStrings.ts
+++ reversingStrings.ts
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 function reverseString(str) {
     return str.split('').reverse().join('');
 }
 console.log(reverseString("stressed"));
+=======
+ function reverseString(str: string): string {
+    return str.split('').reverse().join('');
+}
+console.log(reverseString("strops")); 
+>>>>>>> d98a5b3e9c971f97fa42cc92f3f77b7871259e71
