09a1460e7154b911fd1fcd30f4827543b18232a4
michele <michele@beehive@gmail.com>
2025-05-21 09:31:02 +0200

    update_Allergies

--- Allergies.ts
+++ Allergies.ts
@@ -1,44 +1,71 @@
+import inquirer from 'inquirer';
+
 class Allergies {
-    id:number;
-    medical_records:number;
-    id_patient:number;
-    allergen:string;
-    reaction;string;
-    severity:string;
-    note:string;
-    start_date:Date;
-    end_date:Date;
-    
+  id: number;
+  medical_records: number;
+  id_patient: number;
+  allergen: string;
+  reaction: string;
+  severity: string;
+  note: string;
+  start_date: Date;
+  end_date: Date;
 
-    constructor (id:number,id_patient:number, medical_records:number, allergen:string,reaction:string,severity:string,note:string,start_date:Date,end_date:Date) {
-        this.id=id;
-        this.id_patient=id_patient;
-        this.medical_records=medical_records;
-        this.allergen=allergen;
-        this.reaction=reaction;
-        this.severity=severity;
-        this.note=note;
-        this.start_date=start_date;
-        this.end_date=end_date;
-        
+  constructor(
+    id: number,
+    id_patient: number,
+    medical_records: number,
+    allergen: string,
+    reaction: string,
+    severity: string,
+    note: string,
+    start_date: Date,
+    end_date: Date
+  ) {
+    this.id = id;
+    this.id_patient = id_patient;
+    this.medical_records = medical_records;
+    this.allergen = allergen;
+    this.reaction = reaction;
+    this.severity = severity;
+    this.note = note;
+    this.start_date = start_date;
+    this.end_date = end_date;
+  }
 }
+
+// Oggetto di esempio
+let allergen = new Allergies(
+  1,
+  1,
+  1,
+  'peanuts',
+  'swelling of the hands',
+  'medium',
+  'no note',
+  new Date(2003, 10, 1), // Mese 10 = novembre
+  new Date()
+);
+
+async function main() {
+  const answers = await inquirer.prompt([
+    {
+      type: 'checkbox',
+      name: 'action',
+      message: "Vuoi 'visualizzare' o 'eliminare' l'allergene?",
+      choices: ['visualizzare', 'eliminare']
+    }
+  ]);
+
+  if (answers.action.includes('visualizzare')) {
+    console.log('Allergia:', allergen);
+    console.log(answers.azione);
+  }
+
+  if (answers.action.includes('eliminare')) {
+    allergen.allergen = '';
+    console.log('Allergene eliminato');
+  }
 }
-let allergen = new Allergies(1, 1, 1, "peanuts", "swelling of the hands", "medium", "no note", new Date(2003,10,2003), new Date());
-console.log(allergen);
 
-allergen.allergen = "";
-console.log(allergen);
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
+main();
